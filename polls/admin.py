5c9106b83e91eab9675d2b4c9f3ec269b24aecf1
88dldl <chltnrgml0999@gmail.com>
2024-09-19 17:42:49 +0900

    5주차 : 어드민 페이지 수정

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    # Question 추가 페이지 내 항목들의 순서 변경(지정)
+    fields = ['pub_date', 'question_text'] # fields 변수명은 고정입니다.
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
