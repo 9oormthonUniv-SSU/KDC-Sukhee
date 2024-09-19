ca8a37bc93d4b8fcc9953a04ca4b66f76ded4ea4
88dldl <chltnrgml0999@gmail.com>
2024-09-19 17:44:40 +0900

    5주차 : Inline 영역 간결하게 수정 및 중요하지 않는 정보 가리기

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Question, Choice
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 2
 
@@ -8,8 +8,7 @@ class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         # ('field 집합의 소제목', {'fields': ['field 이름 1', 'field 이름 2', ...]}),
         ("Question title", {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
-    ]
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),    ]
     inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
